6a24dbe3bd8bca0588d6ab2b5da498369bf52947
Johann Böhler <johann.boehler@googlemail.com>
2015-06-05 23:14:19 +0200

    started to replace duplicate implementation of idea UI cards by angular directives

---
+++ el-bombillo-ui/src/main/resources/webapp/app/shared/directive/ideaCardDirective.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../types/tsd.d.ts"/>
+/// <reference path="../../app.module.ts"/>
+
+module directive {
+    'use strict';
+
+    export class IdeaCardDirective implements ng.IDirective {
+        restrict: string = 'A';
+        templateUrl: string = 'idea-card-directive.html';
+        scope = {
+
+            // @property -> read only
+            // =property -> two way binding
+
+            editable: '@editable',
+            idea: '=idea'
+        };
+    }
+
+    angular
+        .module("app")
+        .directive('ideaCardDirective', () => new IdeaCardDirective());
+};
