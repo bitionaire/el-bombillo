79683a7d8fb3cab687050b7a6b25803c62d1c00b
Tobias Lindenmann <tobias.lindenmann@1und1.de>
2015-06-13 00:04:28 +0200

    added validation

--- el-bombillo-ms-idea/src/main/java/org/bitionaire/elbombillo/ms/idea/configuration/IdeaServerConfiguration.java
+++ el-bombillo-ms-idea/src/main/java/org/bitionaire/elbombillo/ms/idea/configuration/IdeaServerConfiguration.java
@@ -4,12 +4,17 @@ package org.bitionaire.elbombillo.ms.idea.configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
+import javax.validation.Valid;
+
 /**
  * @author netdevfighter
  */
+
 public class IdeaServerConfiguration {
 
     @Getter
+    @Valid
     @JsonProperty("server")
-    private ServerFactory serverFactoryr;
+    private ServerFactory serverFactory;
+
 }
--- el-bombillo-ms-idea/src/main/java/org/bitionaire/elbombillo/ms/idea/configuration/ServerFactory.java
+++ el-bombillo-ms-idea/src/main/java/org/bitionaire/elbombillo/ms/idea/configuration/ServerFactory.java
@@ -1,7 +1,12 @@
 package org.bitionaire.elbombillo.ms.idea.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.advantageous.qbit.server.EndpointServerBuilder;
 import io.advantageous.qbit.server.ServiceEndpointServer;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 
 /**
  * @author netdevfighter
@@ -11,11 +16,15 @@ public class ServerFactory {
     @JsonProperty
     private String hostname = "localhost";
 
+    @Min(1)
+    @Max(65535)
     @JsonProperty
-    private int port = 8080;
+    private int port;
 
     public ServiceEndpointServer createServer() {
-
-        return null;
+        EndpointServerBuilder serverBuilder = EndpointServerBuilder.endpointServerBuilder();
+        serverBuilder.setHost(hostname);
+        serverBuilder.setPort(port);
+        return serverBuilder.build();
     }
 }
