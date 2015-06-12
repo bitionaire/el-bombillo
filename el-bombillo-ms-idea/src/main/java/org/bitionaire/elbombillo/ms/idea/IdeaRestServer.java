81ed9f0b1e3fdd25ba36088dde5a22c48763411d
Tobias Lindenmann <tobias.lindenmann@1und1.de>
2015-06-12 11:45:59 +0200

    next small steps

---
+++ el-bombillo-ms-idea/src/main/java/org/bitionaire/elbombillo/ms/idea/IdeaRestServer.java
@@ -0,0 +1,13 @@
+package org.bitionaire.elbombillo.ms.idea;
+
+/**
+ *
+ *
+ * @author Tobias lindenmann
+ */
+public class IdeaRestServer {
+
+    public static void  main(String[] args) {
+
+    }
+}
