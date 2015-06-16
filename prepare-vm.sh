26bdbcc95931ef076b85d515cbacf88fb040ed6d
Johann Böhler <johann.boehler@googlemail.com>
2015-06-16 19:41:35 +0200

    fixed ansible installation of initial vagrant

--- prepare-vm.sh
+++ prepare-vm.sh
@@ -4,13 +4,18 @@ echo "[${LOG_NAME}] Provisioning started"
 
 # Install ansible
 if ! [ -e /opt/ansible ]; then
+    echo "[${LOG_NAME}] About to install from scratch"
+    sudo -s
+    yum -y install git gcc python-devel
     cd /opt
     git clone git://github.com/ansible/ansible.git --recursive
     cd /opt/ansible
     source /opt/ansible/hacking/env-setup
-    sudo easy_install pip
-    sudo pip install paramiko PyYAML Jinja2 httplib2
+    easy_install pip
+    pip install paramiko PyYAML Jinja2 httplib2
 else
+    echo "[${LOG_NAME}] About to update ansible installation"
+    sudo -s
     cd /opt/ansible
     git pull --rebase
     git submodule update --init --recursive
