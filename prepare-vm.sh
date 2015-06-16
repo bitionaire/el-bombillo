6417751c145c8f9123c11616b83a7c005a423a57
Johann Böhler <johann.boehler@googlemail.com>
2015-06-16 20:47:27 +0200

    installation fixes

--- prepare-vm.sh
+++ prepare-vm.sh
@@ -2,23 +2,39 @@
 readonly LOG_NAME="EL-BOMBILLO"
 echo "[${LOG_NAME}] Provisioning started"
 
+sudo -s
+
+# Install tools for general availability
+yum -y install git gcc python2-devel rpm-build asciidoc epel-release device-mapper-event-libs docker
+
 # Install ansible
 if ! [ -e /opt/ansible ]; then
-    echo "[${LOG_NAME}] About to install from scratch"
-    sudo -s
-    yum -y install git gcc python-devel
+    echo "[${LOG_NAME}] About to install ansible from scratch"
     cd /opt
     git clone git://github.com/ansible/ansible.git --recursive
     cd /opt/ansible
-    source /opt/ansible/hacking/env-setup
-    easy_install pip
-    pip install paramiko PyYAML Jinja2 httplib2
+    make rpm
+    yum localinstall -y ./rpm-build/ansible-*.noarch.rpm
 else
-    echo "[${LOG_NAME}] About to update ansible installation"
-    sudo -s
     cd /opt/ansible
-    git pull --rebase
-    git submodule update --init --recursive
+    if ! git diff-index --quiet HEAD --; then
+        echo "[${LOG_NAME}] No ansible update available"
+    else
+        echo "[${LOG_NAME}] About to update ansible installation"
+        git pull --rebase
+        git submodule update --init --recursive
+        make rpm
+        yum localinstall -y ./rpm-build/ansible-*.noarch.rpm
+    fi
+fi
+
+# Start docker docker
+if [ "$(systemctl is-active docker)" != "active" ] ; then
+    echo "[${LOG_NAME}] About to start docker"
+    systemctl start docker
+    chkconfig docker on
+else
+    echo "[${LOG_NAME}] Docker already running"
 fi
 
 echo "[${LOG_NAME}] Provisioning successfully finished"
