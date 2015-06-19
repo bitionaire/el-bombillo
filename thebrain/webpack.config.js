d10f44f8633479eba144f5535bde8490b4ffbe91
Michael Müller <haimich@gmx.net>
2015-06-19 15:28:55 +0000

    making webpack work

--- thebrain/webpack.config.js
+++ thebrain/webpack.config.js
@@ -1,23 +1,26 @@
 var webpack = require('webpack');
 
 module.exports = {
+  entry: {
+    'thebrain': './src/js/main.js'
+  },
+  output: {
+    filename: 'static/js/[name].js'
+  },
+
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      // {test: /\.css$/, loader: 'style!css'},
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/}
+    ]
+  },
+
   devServer: {
     contentBase: "./static",
     noInfo: true,
     hot: false,
     inline: false
   }
+
 };
-//   entry: {
-//     'thebrain': './src/js/main.js'
-//   },
-//   output: {
-//     filename: '[name].js'
-//   },
-//   devtool: 'source-map',
-//   module: {
-//     loaders: [
-//       {test: /\.css$/, loader: 'style!css'},
-//       {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/}
-//     ]
-//   }
