541c98d0123f374ef9ad428509aa4d9372d79e45
Johann Böhler <johann.boehler@1und1.de>
2015-07-24 13:28:00 +0200

    fixed queries

--- el-bombillo-account-service/src/main/java/org/bitionaire/elbombillo/account/jdbi/AccountDAO.java
+++ el-bombillo-account-service/src/main/java/org/bitionaire/elbombillo/account/jdbi/AccountDAO.java
@@ -9,13 +9,13 @@ import java.util.List;
 @RegisterMapper(AccountMapper.class)
 public interface AccountDAO {
 
-    @SqlQuery("select id, username, firstname, lastname, email from bombillo_user where id = :id")
+    @SqlQuery("select id, username, firstname, lastname, email from account where id = :id")
     Account findUser(@Bind("id") long id);
 
-    @SqlQuery("select id, username, firstname, lastname, email from bombillo_user")
+    @SqlQuery("select id, username, firstname, lastname, email from account")
     List<Account> allUsers();
 
     @GetGeneratedKeys
-    @SqlUpdate("insert into bombillo_user (username, firstname, lastname, email) values (:username, :firstName, :lastName, :email)")
+    @SqlUpdate("insert into account (username, firstname, lastname, email) values (:username, :firstName, :lastName, :email)")
     long insertUser(@BindBean Account account);
 }
